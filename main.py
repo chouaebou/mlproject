55620ab381d72667013ea6d248b16c8092f74b21
Chouaebou Mafera <imounchigam@gmail.com>
2025-03-08 13:22:04 -0500

    Model Trainer

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import sys
 from src.logging.logger import logging
+from src.components.model_trainer import ModelTrainer
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 
 if __name__ == "__main__":
-    # Get data ingestion and initiate
-    data_ingestion = DataIngestion()    
-    train_data, test_data = data_ingestion.initiate_data_ingestion()
-    # Get data transformed
+    # Declaration of variables
+    model_trainer = ModelTrainer()
+    data_ingestion = DataIngestion() 
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    # Get data ingestion and initiate
+    train_path, test_path = data_ingestion.initiate_data_ingestion()
+    
+    # Get data transformed    
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path, test_path)
+    
+    # Get model trainer
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
