d1c99baef88cf240ffc675eec4d765876d03575c
Chouaebou Mafera <imounchigam@gmail.com>
2025-03-10 10:14:59 -0400

    Deployment configuration

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -45,3 +45,19 @@ class DataIngestion:
             )
         except Exception as e:
             raise CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    # Declaration of variables
+    model_trainer = ModelTrainer()
+    data_ingestion = DataIngestion() 
+    data_transformation = DataTransformation()
+    
+    # Get data ingestion and initiate
+    train_path, test_path = data_ingestion.initiate_data_ingestion()
+    
+    # Get data transformed    
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_path, test_path)
+    
+    # Get model trainer
+    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
